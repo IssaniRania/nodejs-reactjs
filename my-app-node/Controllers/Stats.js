9fdd38052b1b65cf7109b31d3d9b30cd638cf9cb
IssaniRania <raniaissani83@gmail.com>
2024-02-21 12:54:03 +0100

    new notif

--- my-app-node/Controllers/Stats.js
+++ my-app-node/Controllers/Stats.js
@@ -16,3 +16,21 @@ exports.create = (req, res) => {
       .then(stats => res.status(200).json(stats))
       .catch(err => res.status(400).json({ error: err.message }));
   };
+  //notification
+  io.on('connection', (socket) => {
+    console.log('Client connecté');
+  
+    socket.on('disconnect', () => {
+      console.log('Client déconnecté');
+    });
+  });
+  
+  // Fonction pour vérifier et envoyer des notifications
+  function checkAndNotify(value) {
+    const VALEUR_MINIMALE = 10
+  
+    // Vérifiez si la valeur est inférieure à la valeur minimale souhaitée
+    if (value < VALEUR_MINIMALE) {
+      io.emit('notification', { message: 'La valeur minimale a été atteinte !' });
+    }
+  }
